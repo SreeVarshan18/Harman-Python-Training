cbc37ecc759a733528692e23e6b0ec02a8a87f13
SreeVarshan18 <varshan.sree18@gmail.com>
2022-03-03 14:37:34 +0530

    break continue and pass

---
+++ bcp.py
@@ -0,0 +1,23 @@
+i = 0
+while i <= 9:
+    i += 1
+    if i == 3:
+        continue
+
+    print(i)
+j = 0
+while j <= 9:
+    if j == 3:
+        break
+    print(j)
+    j += 1
+
+
+k = 0
+while k <= 9:
+    if k == 3:
+        pass
+    print(k)
+    k += 1
+
+
